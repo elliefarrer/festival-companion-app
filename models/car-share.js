4e27a21c3d6767ccc95b5308b101224105d24203
Ellie Farrer <elliefarrermusic@gmail.com>
2018-08-16 16:25:40 +0100

    Added virtual and helper comments to model

--- models/car-share.js
+++ models/car-share.js
@@ -6,22 +6,23 @@ const carShareSchema = mongoose.Schema({
   festivalStartDate: { type: mongoose.Schema.ObjectId, ref: 'Festival' },
   festivalEndDate: { type: mongoose.Schema.ObjectId, ref: 'Festival' },
   rideStartTime: { type: String, required: true },
+  estimatedRideEndTime: { type: String, required: true }, // get from MapQuest/Direction API
   from: {
-    postcode: { type: String, required: true },
-    lat: Number,
-    lng: Number
+    postcode: { type: String, required: true }, // put into Nominatim
+    lat: Number, // get from Nominatim
+    lng: Number  // get from Nominatim
   },
   to: {
-    postcode: { type: String, required: true },
-    lat: Number,
-    lng: Number
+    postcode: { type: String, required: true }, // put into Nominatim
+    lat: Number, // get from Nominatim
+    lng: Number // get from Nominatim
   },
   stopPoints: [{
     passengerName: String,
     arrangedTime: String,
-    postcode: String,
-    lat: Number,
-    lng: Number
+    postcode: String, // put into Nominatim
+    lat: Number, // get from Nominatim
+    lng: Number // get from Nominatim
   }],
   price: Number,
   totalSeats: { type: Number, required: true },
@@ -33,5 +34,11 @@ const carShareSchema = mongoose.Schema({
     timeStamp: String
   }]
 });
+
+carShareSchema.virtual('seatsRemaining')
+  .get(function() {
+    return this.totalSeats - this.numOfPassengers;
+  });
+
 
 module.exports = mongoose.model('CarShare', carShareSchema);
