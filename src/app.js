bef9c23f5e2bd423a58badffe83d8d21a1c9b291
Ellie Farrer <elliefarrermusic@gmail.com>
2018-08-20 15:01:11 +0100

    Wrote login controller

--- src/app.js
+++ src/app.js
@@ -15,9 +15,12 @@ import FestivalsNewCtrl from './controllers/festivals/new';
 import FestivalsEditCtrl from './controllers/festivals/edit';
 import UsersShowCtrl from './controllers/users/show';
 import UsersEditCtrl from './controllers/users/edit';
+import AuthLoginCtrl from './controllers/auth/login';
+import AuthRegisterCtrl from './controllers/auth/register';
+import AuthRegisterOrgCtrl from './controllers/auth/register-organiser';
 
 angular.module('Festival Companion App', [
-  'ui.router'
+  'ui.router', 'satellizer'
 ])
   .controller('CarSharesIndexCtrl', CarSharesIndexCtrl)
   .controller('CarSharesShowCtrl', CarSharesShowCtrl)
@@ -29,4 +32,12 @@ angular.module('Festival Companion App', [
   .controller('FestivalsEditCtrl', FestivalsEditCtrl)
   .controller('UsersShowCtrl', UsersShowCtrl)
   .controller('UsersEditCtrl', UsersEditCtrl)
-  .config(Router);
+  .controller('AuthLoginCtrl', AuthLoginCtrl)
+  .controller('AuthRegisterCtrl', AuthRegisterCtrl)
+  .controller('AuthRegisterOrgCtrl', AuthRegisterOrgCtrl)
+  .config(Router)
+  .config(function($authProvider){
+    $authProvider.loginUrl = '/api/login';
+    $authProvider.registerUrl = '/api/register';
+    $authProvider.registerOrgUrl = '/api/registerorg';
+  });
