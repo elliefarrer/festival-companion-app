c0ea6b0791a287d52ebdbb729cbb65d7e90cb310
curtisburns <curtis.burns28@gmail.com>
2018-08-20 02:00:10 +0100

    got the passenger request system completed and functional, arrays behaving as expected - may need more testing

--- controllers/carShareController.js
+++ controllers/carShareController.js
@@ -1,11 +1,27 @@
 // if statement on front end will filter car shares according to the festival.
 
 const CarShare = require('../models/car-share');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/environment');
+let token;
+let userId;
 
-function carSharesIndex(req, res, next) {
+function getTokenFromHttpRequest(req) {
+  token = req.headers.authorization.replace('Bearer ', '');
+  function retrieveUserIdFromToken(err, result) {
+    userId = result.sub;
+  }
+  jwt.verify(token, secret, retrieveUserIdFromToken);
+}
+
+function carSharesIndex(req, res, next) { //Only brings back carShares related to that festival.
+  let relevantCarShares;
   CarShare
     .find()
-    .then(carShares => res.json(carShares))
+    .then(carShares => relevantCarShares = carShares.filter(carShare =>
+      carShare.festival.toString() === req.params.id))
+    .then(() => res.json(relevantCarShares))
     .catch(next);
 }
 
@@ -17,9 +33,22 @@ function carSharesShow(req, res, next) {
 }
 
 function carSharesCreate(req, res, next) {
+  let carShareId;
+  getTokenFromHttpRequest(req);
+  req.body.createdBy = userId;
   CarShare
     .create(req.body)
-    .then(carShare => res.status(201).json(carShare))
+    .then(carShare => {
+      carShareId = carShare.id;
+      return User.findById(userId);
+    })
+    .then(user => {
+      user.carSharesOrganised.push(carShareId);
+      console.log(user);
+      return user.save();
+    })
+    .then(() => CarShare.findById(carShareId))
+    .then((carShare) => res.status(201).json(carShare))
     .catch(next);
 }
 
@@ -39,10 +68,22 @@ function carSharesUpdate(req, res) {
     .catch(() => res.status(500).json({ message: 'Something went wrong'}));
 }
 
-function carSharesDelete(req, res, next) {
+function carSharesDelete(req, res, next) { // deletes from carSharesOrganised as well
+  const carShareId = req.params.carShareId;
+  let createdBy;
   CarShare
     .findById(req.params.carShareId)
-    .then(carShare => carShare.remove())
+    .then(carShare => {
+      createdBy = carShare.createdBy;
+      return carShare.remove();
+    })
+    .then(() => User.findById(createdBy))
+    .then(user => {
+      user.carSharesOrganised = user.carSharesOrganised.filter(id =>
+        id.toString() !== carShareId
+      );
+      return user.save();
+    })
     .then(() => res.sendStatus(204))
     .catch(next);
 }
--- test/passenger/new_spec.js
+++ test/passenger/new_spec.js
@@ -5,15 +5,11 @@ const CarShare = require('../../models/car-share');
 const Festival = require('../../models/festival');
 const { secret } = require('../../config/environment');
 const jwt = require('jsonwebtoken');
-let token;
 
 
-const userData = [
-  {firstName: 'Ellie', lastName: 'Farrer', userName: 'ellie', email: 'ellie@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'},
-  {firstName: 'Curtis', lastName: 'Burns', userName: 'curtis', email: 'curtis@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'},
-  {firstName: 'Max', lastName: 'Cramer', userName: 'max', email: 'max@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'}
-];
-
+const userData = [{
+  firstName: 'Jerry', lastName: 'Springer', userName: 'Jezza', email: 'jezza@email.com',  mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+}];
 
 const carShareData = {
   from: {
@@ -38,109 +34,271 @@ const festivalData = {
 };
 
 describe('POST /festivals/:festivalId/carShares/:carShareId/passengers', () => {
-  let festivalId;
-  let carShareId;
-  const userId = [];
-  let loggedInId;
 
   beforeEach(done => {
-    // Festival.remove({})
-    //   .then(() => {
-    //     return CarShare.remove({})
-    //   })
     Promise.all([
       Festival.remove({}),
       CarShare.remove({}),
       User.remove({})
     ])
-    // TODO PICK ONE OF THE ABOVE
       .then(() => Festival.create(festivalData))
 
       .then(festival => {
-        festivalId = festival._id;
-        carShareData.festival = festivalId;
+        carShareData.festival = festival.id;
         return CarShare.create(carShareData);
       })
-      .then((carShare) => {
-        carShareId = carShare._id;
-        console.log('carshare id is', carShareId);
-        return User.create(userData);
-      })
-      .then(users => {
-
-        loggedInId = users[0].id; //why does filter not work? Index 0?
-        console.log('user is', users[0]);
-        token = jwt.sign({ sub: loggedInId }, secret, { expiresIn: '1hr' });
-        userId[0] = loggedInId;
-
-        userId[1] = users[1].id;
-
-        userId[2] = users[2].id;
-
-        return CarShare.find();
-      })
-      .then(carShare => {
-        for ( let i = 0; i <= 2; i++) {
-          // console.log(userId[i]);
-          // console.log(carShare[0].passengers);
-          carShare[0].passengers.push(userId[i]); //why is do I have to index 0?
-        }
-        carShare.save();
-      });
+      .then(() => User.create(userData));
     done();
   });
 
-  xit('should return a 401 without a token', done => {
-    api.post(`/api/festivals/${festivalId}/carShares/${carShareId}/passengers`)
+  it('should return a 401 without a token', done => {
+    const festival = new Festival({
+      name: 'LoveBox',
+      startDate: '13th July',
+      endDate: '14th July',
+      location: 'Gunnersbury Park',
+      camping: 'No',
+      headlining: ['Childish Gambino', 'Skepta', 'SZA'],
+      photoUrl: 'https://24e8e3b95851cffc9b46-ce987c743c8a722dc56cea7f8eb55a8f.ssl.cf3.rackcdn.com/LBXLogoSimple.svg'
+
+    });
+    festival.save();
+    // console.log('my festival id is', festival.id);
+
+    const carShare = new CarShare({
+      from: {
+        postcode: 'W2 1HQ',
+        lat: 51.5167,
+        lng: 0.1769
+      },
+      rideStartTime: '08.00PM',
+      estimatedRideEndTime: '3.30PM',
+      passengers: []
+    });
+    carShare.save();
+    // console.log('my carShare id is', carShare.id);
+
+
+    api.post(`/api/festivals/${festival.id}/carShares/${carShare.id}/passengers`)
       .end((err, res) => {
         expect(res.status).to.eq(401);
         done();
       });
   });
 
-  xit('should return a 200 with a token', done => {
-    api.post(`/api/festivals/${festivalId}/carShares/${carShareId}/passengers`)
-      .set('Authorization', `Bearer ${token}`) // Create an authorization header
+  it('should return a 201 with a token', done => {
+    const festival = new Festival({
+      name: 'LoveBox',
+      startDate: '13th July',
+      endDate: '14th July',
+      location: 'Gunnersbury Park',
+      camping: 'No',
+      headlining: ['Childish Gambino', 'Skepta', 'SZA'],
+      photoUrl: 'https://24e8e3b95851cffc9b46-ce987c743c8a722dc56cea7f8eb55a8f.ssl.cf3.rackcdn.com/LBXLogoSimple.svg'
+
+    });
+    festival.save();
+    // console.log('my festival id is', festival.id);
+
+    const carShare = new CarShare({
+      from: {
+        postcode: 'W2 1HQ',
+        lat: 51.5167,
+        lng: 0.1769
+      },
+      rideStartTime: '08.00PM',
+      estimatedRideEndTime: '3.30PM',
+      passengers: []
+    });
+    carShare.save();
+
+    const user = new User({
+      firstName: 'Ellie', lastName: 'Farrer', userName: 'ellie', email: 'ellie@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+    });
+
+    user.save();
+    const token = jwt.sign({ sub: user.id, admin: false }, secret, { expiresIn: '1hr' });
+    console.log(token, user.id, `/api/festivals/${festival.id}/carShares/${carShare.id}/passengers`);
+
+    api.post(`/api/festivals/${festival.id}/carShares/${carShare.id}/passengers`)
+      .set('Authorization', `${token}`) // Create an authorization header
       .end((err, res) => {
-        console.log('this is the response', res.body);
-        expect(res.status).to.eq(200);
+        // console.log('this is the response', res.body);
+        expect(res.status).to.eq(201);
         done();
       });
   });
-  //doesn't work - need to get past this. Works in insomnia
-
-  xit('should return an array', done => {
-    api.get(`/api/festivals/${festivalId}/carShares/${carShareId}/passengers`)
-      .set('Authorization', `Bearer ${token}`) // Create an authorization header
-      .end((err, res) => {
-        console.log('this is the response', res.body);
-        expect(res.body).to.be.an('array');
-        done();
-      });
-  });
-
-  xit('should return an array of objects', done => {
-    api.get(`/api/festivals/${festivalId}/carShares/${carShareId}/passengers`)
-      .set('Authorization', `Bearer ${token}`) // Create an authorization header
-      .end((err, res) => {
-        res.body.forEach(passenger => {
-          expect(passenger).to.be.an('object');
-        });
-        done();
-      });
-  });
-
-  xit('should return the correct data', done => {
-    api.get(`/api/festivals/${festivalId}/carShares/${carShareId}/passengers`)
-      .set('Authorization', `Bearer ${token}`) // Create an authorization header
-      .end((err, res) => {
-        res.body.forEach(carShare => {
-          expect(carShare.pendingPassenger[0].id).to.eq(loggedInId);
-        });
-        expect(res.body.length).to.eq(1);
-      });
-    done();
-  });
 
 
+  // it('should return an array of the correct length',
+  //   done => {
+  //     const user = new User({
+  //       firstName: 'Ellie', lastName: 'Farrer', userName: 'ellie', email: 'ellie@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //     });
+  //     user.save();
+  //
+  //
+  //     const token = jwt.sign({ sub: user.id, admin: false }, secret, { expiresIn: '1hr' });
+  //
+  //
+  //     const festival = new Festival({
+  //       name: 'LoveBox',
+  //       startDate: '13th July',
+  //       endDate: '14th July',
+  //       location: 'Gunnersbury Park',
+  //       camping: 'No',
+  //       headlining: ['Childish Gambino', 'Skepta', 'SZA'],
+  //       photoUrl: 'https://24e8e3b95851cffc9b46-ce987c743c8a722dc56cea7f8eb55a8f.ssl.cf3.rackcdn.com/LBXLogoSimple.svg'
+  //
+  //     });
+  //     festival.save();
+  //     // console.log('my festival id is', festival.id);
+  //
+  //     const carShare = new CarShare({
+  //       from: {
+  //         postcode: 'W2 1HQ',
+  //         lat: 51.5167,
+  //         lng: 0.1769
+  //       },
+  //       rideStartTime: '08.00PM',
+  //       estimatedRideEndTime: '3.30PM',
+  //       passengers: []
+  //     });
+  //
+  //     carShare.save();
+  //     // console.log('my carShare id is', carShare.id);
+  //
+  //     api.post(`/api/festivals/${festival.id}/carShares/${carShare.id}/passengers/`)
+  //
+  //       .set('Authorization', `Bearer ${token}`)
+  //       .end((err, res) => {
+  //         console.log('this is the second token', token);
+  //         // console.log('the res is', res.body);
+  //         expect(res.body.length).to.eq(3);
+  //         done();
+  //       });
+  //   });
+  //
+  // xit('should return an array of objects', done => {
+  //
+  //   const user1 = new User({
+  //     firstName: 'Ellie', lastName: 'Farrer', userName: 'ellie', email: 'ellie@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //   });
+  //   user1.save();
+  //   const user2 = new User({
+  //     firstName: 'Curtis', lastName: 'Burns', userName: 'curtis', email: 'curtis@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //   });
+  //   user2.save();
+  //   const user3 = new User({
+  //     firstName: 'Max', lastName: 'Cramer', userName: 'max', email: 'max@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //   });
+  //   user3.save();
+  //
+  //   const token = jwt.sign({ sub: user1.id, admin: false }, secret, { expiresIn: '1hr' });
+  //
+  //   const festival = new Festival({
+  //     name: 'LoveBox',
+  //     startDate: '13th July',
+  //     endDate: '14th July',
+  //     location: 'Gunnersbury Park',
+  //     camping: 'No',
+  //     headlining: ['Childish Gambino', 'Skepta', 'SZA'],
+  //     photoUrl: 'https://24e8e3b95851cffc9b46-ce987c743c8a722dc56cea7f8eb55a8f.ssl.cf3.rackcdn.com/LBXLogoSimple.svg'
+  //
+  //   });
+  //   festival.save();
+  //   // console.log('my festival id is', festival.id);
+  //
+  //   const carShare = new CarShare({
+  //     from: {
+  //       postcode: 'W2 1HQ',
+  //       lat: 51.5167,
+  //       lng: 0.1769
+  //     },
+  //     rideStartTime: '08.00PM',
+  //     estimatedRideEndTime: '3.30PM',
+  //     passengers: []
+  //   });
+  //   carShare.passengers.push(user1.id);
+  //   carShare.passengers.push(user2.id);
+  //   carShare.passengers.push(user3.id);
+  //   carShare.save();
+  //   // console.log('my carShare id is', carShare.id);
+  //
+  //   api.post(`/api/festivals/${festival.id}/carShares/${carShare.id}/passengers`)
+  //     .set('Authorization', `Bearer ${token}`) // Create an authorization header
+  //     .end((err, res) => {
+  //       // console.log('this is the body of the res', res.body);
+  //       res.body.forEach(passenger => {
+  //         expect(passenger).to.be.an('object');
+  //       });
+  //       expect(res.body).to.be.an('array');
+  //       done();
+  //     });
+  // });
+  //
+  // xit('should return the correct data', done => {
+  //
+  //   const user1 = new User({
+  //     firstName: 'Ellie', lastName: 'Farrer', userName: 'ellie', email: 'ellie@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //   });
+  //   user1.save();
+  //   const user2 = new User({
+  //     firstName: 'Curtis', lastName: 'Burns', userName: 'curtis', email: 'curtis@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //   });
+  //   user2.save();
+  //   const user3 = new User({
+  //     firstName: 'Max', lastName: 'Cramer', userName: 'max', email: 'max@email.com', mobileNumber: '07770077070', password: 'pass', passwordConfirmation: 'pass'
+  //   });
+  //   user3.save();
+  //
+  //   const token = jwt.sign({ sub: user1.id, admin: false }, secret, { expiresIn: '1hr' });
+  //
+  //   const festival = new Festival({
+  //     name: 'LoveBox',
+  //     startDate: '13th July',
+  //     endDate: '14th July',
+  //     location: 'Gunnersbury Park',
+  //     camping: 'No',
+  //     headlining: ['Childish Gambino', 'Skepta', 'SZA'],
+  //     photoUrl: 'https://24e8e3b95851cffc9b46-ce987c743c8a722dc56cea7f8eb55a8f.ssl.cf3.rackcdn.com/LBXLogoSimple.svg'
+  //
+  //   });
+  //   festival.save();
+  //   // console.log('my festival id is', festival.id);
+  //
+  //   const carShare = new CarShare({
+  //     from: {
+  //       postcode: 'W2 1HQ',
+  //       lat: 51.5167,
+  //       lng: 0.1769
+  //     },
+  //     rideStartTime: '08.00PM',
+  //     estimatedRideEndTime: '3.30PM',
+  //     passengers: []
+  //   });
+  //   carShare.passengers.push(user1.id);
+  //   carShare.passengers.push(user2.id);
+  //   carShare.passengers.push(user3.id);
+  //   carShare.save();
+  //
+  //   const userId = [];
+  //   userId.push(user1.id);
+  //   userId.push(user2.id);
+  //   userId.push(user3.id);
+  //
+  //   // console.log('my carShare id is', carShare.id);
+  //
+  //   api.post(`/api/festivals/${festival.id}/carShares/${carShare.id}/passengers`)
+  //     .set('Authorization', `Bearer ${token}`) // Create an authorization header
+  //     .end((err, res) => {
+  //       res.body.forEach((passenger, index) => {
+  //         // console.log('the passenger is', passenger);
+  //         expect(passenger._id).to.eq(userId[index]);
+  //         // expect(carShare.passenger.some(user1.id)).to.eq(true);
+  //       });
+  //       expect(res.body.length).to.eq(3);
+  //     });
+  //   done();
+  // });
 });
