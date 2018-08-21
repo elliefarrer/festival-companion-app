6d387b8e214fa4dd2787952b87afa962fe8cfcd4
Max Cramer <maximiliancramer@yahoo.com>
2018-08-21 10:18:57 +0100

    Added login and register

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -2,7 +2,10 @@ function AuthRegisterCtrl($auth, $scope, $state) {
   $scope.register = function() {
     $auth
       .signup($scope.user)
-      .then(() => $auth.login($scope.user))
+      .then(res => {
+        console.log('This is the User Data', res.data);
+        $auth.login($scope.user);
+      })
       .then(() => $state.go('festivalsIndex'));
   };
 }
