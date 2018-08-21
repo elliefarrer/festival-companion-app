b8e3a86a50991a70dec57ebbc0346a246e5bf796
Ellie Farrer <elliefarrermusic@gmail.com>
2018-08-21 12:52:49 +0100

    Map tilelayer is working

--- src/directives/map.js
+++ src/directives/map.js
@@ -1,20 +1,37 @@
 /*global L*/
-L.mapquest.key = 'DmK3IjydVb4R9lDw3X08xjNBNVV0WOks';
 
-function Map() {
+function Map($http) {
+  // const API_KEY = 'DmK3IjydVb4R9lDw3X08xjNBNVV0WOks';
   return {
     restrict: 'A',
     link($scope, $element) {
       const domElement = $element[0];
       console.log('The map element is', domElement);
-      const map = L.map(domElement);
-      L.mapquest.map('map', {
-        center: [37.7749, -122.4194],
-        layers: L.mapquest.tileLayer('map'),
-        zoom: 12
-      }).addTo(map);
+      console.log('The scope is', $scope);
+      // const map = L.mapquest.map(domElement);
+      $scope.$watch('festival', function() {
+        if($scope.festival) {
+          const API_KEY = 'DmK3IjydVb4R9lDw3X08xjNBNVV0WOks';
+          L.mapquest.key = API_KEY;
+          console.log('Festival scope is', $scope.festival.location);
+          $http({
+            method: 'GET',
+            url: `http://www.mapquestapi.com/geocoding/v1/address?key=${API_KEY}&location=${$scope.festival.location}`
+          })
+            .then(res => {
+              const place = res.data;
+              console.log('res data is', place);
+              L.mapquest.map('map', {
+                center: [res.data.results[0].locations[0].latLng.lat,     res.data.results[0].locations[0].latLng.lng],
+                layers: L.mapquest.tileLayer('map'),
+                zoom: 13
+              });
+            });
+        }
+      });
     }
   };
+  // const marker = L
 }
 
 export default Map;
