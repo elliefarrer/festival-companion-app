5f707dc9326b49048eff1a3c2c9bd89b58147ab1
curtisburns <curtis.burns28@gmail.com>
2018-08-24 09:47:43 +0100

    add filestack to festivals new and edit views

--- src/controllers/festivals/edit.js
+++ src/controllers/festivals/edit.js
@@ -15,6 +15,26 @@ function FestivalsEditCtrl($http, $scope, $state) {
     url: `/api/festivals/${$state.params.id}`
   })
     .then(res => $scope.festival = res.data);
+
+  $scope.imageUploaded = false;
+
+  const fp = filestack.init('AexgdNExkS7KW6EtgRtLPz'); /* global filestack */
+
+  $scope.uploadImage = function() {
+    fp.pick()
+      .then(res => {
+        // console.log('files are', res.filesUploaded[0].url);
+        $scope.festival.photoUrl = res.filesUploaded[0].url;
+        // console.log(fp);
+        if ($scope.festival.photoUrl) {
+          $scope.imageUploaded = true;
+        }
+        $scope.$apply();
+      })
+      .catch(err => console.log(err));
+  };
 }
+
+
 
 export default FestivalsEditCtrl;
